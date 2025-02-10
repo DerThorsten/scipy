c7e3fa1255453e20e982d267cdd79c726e8b422e
DerThorsten <derthorstenbeier@gmail.com>
2025-02-10 14:08:46 +0100

    arpack

--- scipy/sparse/linalg/_eigen/arpack/ARPACK/SRC/stat.h
+++ scipy/sparse/linalg/_eigen/arpack/ARPACK/SRC/stat.h
@@ -13,9 +13,3 @@ c
      &           tnaupd, tnaup2, tnaitr, tneigh, tngets, tnapps, tnconv,
      &           tcaupd, tcaup2, tcaitr, tceigh, tcgets, tcapps, tcconv,
      &           tmvopx, tmvbx, tgetv0, titref, trvec
-      common /timing/ 
-     &           nopx, nbx, nrorth, nitref, nrstrt,
-     &           tsaupd, tsaup2, tsaitr, tseigt, tsgets, tsapps, tsconv,
-     &           tnaupd, tnaup2, tnaitr, tneigh, tngets, tnapps, tnconv,
-     &           tcaupd, tcaup2, tcaitr, tceigh, tcgets, tcapps, tcconv,
-     &           tmvopx, tmvbx, tgetv0, titref, trvec
